b2346322ab598e57f60a999a3b8618bbf8435e85
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-10-12 09:33:15 +0530

    Add files via upload

---
+++ CSS/scratch/lively.js
@@ -0,0 +1,34 @@
+// function first(){
+//     console.log('frist');
+//     second();
+// }
+
+// function second(){
+//     console.log('second');
+//     third();
+// }
+
+// function third(){
+//     console.log('third');
+// }
+// first();
+
+
+// let age=25;
+const name='preeti', 
+age=20, 
+lecture='gla';
+console.log(lecture);
+
+// alert("hello world");
+// [1,2].forEach(alert);
+
+//Integer, decimal
+// bigint +-2^53-1
+// null
+// undefined
+// Boolean
+
+console.log(`my age is 20`)
+alert(`my age is 20`)
+console.log(typeof(lecture))
